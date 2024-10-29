34f1b72fef9e700399848a6eb18d657b3f1f71ea
dangkhoipham80 <158264478+dangkhoipham80@users.noreply.github.com>
2024-10-30 03:04:41 +0700

    update yob in request

--- demo/src/main/java/com/fengshuisystem/demo/mapper/ConsultationRequestMapper.java
+++ demo/src/main/java/com/fengshuisystem/demo/mapper/ConsultationRequestMapper.java
@@ -9,15 +9,13 @@ import org.mapstruct.Named;
 @Mapper(componentModel = "spring")
 public interface ConsultationRequestMapper {
 
-    // Ánh xạ từ Entity sang DTO, sử dụng phương thức tùy chỉnh để lấy packageId
     @Mapping(source = "packageId", target = "packageId", qualifiedByName = "packageToPackageId")
     ConsultationRequestDTO toDTO(ConsultationRequest consultationRequest);
 
-    // Ánh xạ từ DTO sang Entity, packageId sẽ được gán trong Service Layer
     @Mapping(target = "packageId", ignore = true)
+    @Mapping(source = "yob", target = "yob")
     ConsultationRequest toEntity(ConsultationRequestDTO consultationRequestDTO);
 
-    // Phương thức ánh xạ tùy chỉnh: từ Package Entity sang packageId
     @Named("packageToPackageId")
     default Integer mapPackageToPackageId(com.fengshuisystem.demo.entity.Package packageEntity) {
         return packageEntity != null ? packageEntity.getId() : null;
