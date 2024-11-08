a77f8e0e1990c97fc554e9f82aa40fdc1af101d0
dangkhoipham80 <158264478+dangkhoipham80@users.noreply.github.com>
2024-11-08 22:46:33 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,11 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
 import './App.css';
 import AppRoutes from './routes/AppRoutes';
-import { getToken } from "./service/localStorageService";
 
-const App = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const token = getToken();
-    if (token) {
-      try {
-        const base64Url = token.split('.')[1];
-        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        const jsonPayload = decodeURIComponent(
-          atob(base64)
-            .split('')
-            .map((c) => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
-            .join('')
-        );
-        const decodedToken = JSON.parse(jsonPayload);
-
-        if (decodedToken && decodedToken.scope === 'ROLE_ADMIN') {
-          navigate('/admin-page');
-        }
-      } catch (error) {
-        console.error('Lỗi giải mã token:', error);
-      }
-    }
-  }, [navigate]);
-
-  return <AppRoutes />;
-};
+function App() {
+  return (
+    <AppRoutes />
+  );
+}
 
 export default App;
