6bcd1b7b71fa442defbcd92b14b4a75918f80e70
Huypq-SE <huypqse184931@fpt.edu.vn>
2024-11-08 23:55:46 +0700

    jjj

--- src/admin/animal/AddKoiFish.tsx
+++ src/admin/animal/AddKoiFish.tsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Modal, Button } from 'react-bootstrap';
-import { getToken, setToken } from "../../service/localStorageService";
-import { Checkbox, Divider, Upload, Form, Input } from 'antd';
-import type { CheckboxProps } from 'antd';
+import { Checkbox, Divider, Upload, Form as AntdForm, Input } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 import { ref, uploadString, getDownloadURL } from "firebase/storage";
 import { storage } from "../../firebase/firebase";
 import api from "../../axious/axious";
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 interface Colors {
   id: number;
@@ -17,11 +17,7 @@ interface Colors {
 const CheckboxGroup = Checkbox.Group;
 
 const AddKoiFish: React.FC = () => {
-  const [animalCategoryName, setAnimalCategoryName] = useState("");
-  const [description, setDescription] = useState("");
-  const [origin, setOrigin] = useState("");
   const [animalImages, setAnimalImages] = useState<File[]>([]);
-  const [errorMessages, setErrorMessages] = useState({ name: "", description: "", origin: "" });
   const [showModal, setShowModal] = useState(false);
   const [modalMessage, setModalMessage] = useState("");
   const [modalNotification, setModalNotification] = useState("");
@@ -43,13 +39,8 @@ const AddKoiFish: React.FC = () => {
   const checkAll = plainOptions.length === checkedList.length;
   const indeterminate = checkedList.length > 0 && checkedList.length < plainOptions.length;
 
-  const onChange = (list: number[]) => {
-    setCheckedList(list);
-  };
-
-  const onCheckAllChange: CheckboxProps['onChange'] = (e) => {
-    setCheckedList(e.target.checked ? plainOptions.map(option => option.id) : []);
-  };
+  const onChange = (list: number[]) => setCheckedList(list);
+  const onCheckAllChange = (e: any) => setCheckedList(e.target.checked ? plainOptions.map(option => option.id) : []);
 
   const getAllColors = async (): Promise<Colors[] | null> => {
     try {
@@ -86,38 +77,50 @@ const AddKoiFish: React.FC = () => {
     });
   };
 
-  const handleSubmit = async () => {
-    setErrorMessages({ name: "", description: "", origin: "" });
+  const formik = useFormik({
+    initialValues: {
+      animalCategoryName: "",
+      description: "",
+      origin: ""
+    },
+    validationSchema: Yup.object({
+      animalCategoryName: Yup.string()
+        .required("Animal category name is required")
+        .min(3, "Must be at least 3 characters")
+        .max(50, "Must be 50 characters or less"),
+      description: Yup.string()
+        .max(200, "Description can be up to 200 characters"),
+      origin: Yup.string()
+        .required("Origin is required")
+        .min(2, "Must be at least 2 characters")
+        .max(30, "Must be 30 characters or less")
+    }),
+    onSubmit: async (values) => {
+      const base64Avatars = animalImages.length > 0 ? await uploadImagesToFirebase(animalImages) : [];
 
+      try {
+        const response = await api.post("/animals", {
+          ...values,
+          animalImages: base64Avatars.map(url => ({ imageUrl: url })),
+          colors: checkedList.map(id => ({ id }))
+        });
 
-    const base64Avatars = animalImages.length > 0 ? await uploadImagesToFirebase(animalImages) : [];
-
-    try {
-      const response = await api.post("/animals", {
-        animalCategoryName,
-        description,
-        origin,
-        animalImages: base64Avatars.map(url => ({ imageUrl: url })),
-        colors: checkedList.map(id => ({ id }))
-      });
-
-      if (response.data.code !== 1000) {
-        throw new Error(response.data.message);
-      } else {
-        setModalMessage("Animal created successfully");
-        setModalNotification("Success");
-        setModalNotificationColor("green");
+        if (response.data.code !== 1000) {
+          throw new Error(response.data.message);
+        } else {
+          setModalMessage("Animal created successfully");
+          setModalNotification("Success");
+          setModalNotificationColor("green");
+        }
+      } catch (error) {
+        setModalMessage("An error occurred while creating the animal");
+        setModalNotification("Failed");
+        setModalNotificationColor("red");
+      } finally {
+        setShowModal(true);
       }
-    } catch (error) {
-      setModalMessage("An error occurred while creating the animal");
-      setModalNotification("Failed");
-      setModalNotificationColor("red");
-    } finally {
-      setShowModal(true);
     }
-  };
-
-
+  });
 
   const handleUploadChange = (info: any) => {
     const { fileList } = info;
@@ -133,41 +136,35 @@ const AddKoiFish: React.FC = () => {
   return (
     <div className="container">
       <h1 className="mt-5">Koi Fish</h1>
-      <Form onFinish={handleSubmit}>
-        <Form.Item
-          label="Name"
-          rules={[{ required: true, message: "Please input the name of the Koi fish" }]}
-        >
+      <form onSubmit={formik.handleSubmit}>
+        <AntdForm.Item label="Name">
           <Input
-            value={animalCategoryName}
-            onChange={(e) => setAnimalCategoryName(e.target.value)}
+            {...formik.getFieldProps('animalCategoryName')}
           />
-          {errorMessages.name && <div className="error-message">{errorMessages.name}</div>}
-        </Form.Item>
+          {formik.touched.animalCategoryName && formik.errors.animalCategoryName ? (
+            <div className="error-message">{formik.errors.animalCategoryName}</div>
+          ) : null}
+        </AntdForm.Item>
 
-        <Form.Item
-          label="Description"
-          rules={[{ required: true, message: "Please input a description for the Koi fish" }]}
-        >
+        <AntdForm.Item label="Description">
           <Input
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
+            {...formik.getFieldProps('description')}
           />
-          {errorMessages.description && <div className="error-message">{errorMessages.description}</div>}
-        </Form.Item>
+          {formik.touched.description && formik.errors.description ? (
+            <div className="error-message">{formik.errors.description}</div>
+          ) : null}
+        </AntdForm.Item>
 
-        <Form.Item
-          label="Origin"
-          rules={[{ required: true, message: "Please input the origin of the Koi fish" }]}
-        >
+        <AntdForm.Item label="Origin">
           <Input
-            value={origin}
-            onChange={(e) => setOrigin(e.target.value)}
+            {...formik.getFieldProps('origin')}
           />
-          {errorMessages.origin && <div className="error-message">{errorMessages.origin}</div>}
-        </Form.Item>
+          {formik.touched.origin && formik.errors.origin ? (
+            <div className="error-message">{formik.errors.origin}</div>
+          ) : null}
+        </AntdForm.Item>
 
-        <Form.Item label="Avatars" className="form-item">
+        <AntdForm.Item label="Avatars">
           <Upload
             multiple
             accept="image/*"
@@ -181,9 +178,9 @@ const AddKoiFish: React.FC = () => {
               <div style={{ marginTop: 8 }}>Upload</div>
             </div>
           </Upload>
-        </Form.Item>
+        </AntdForm.Item>
 
-        <Form.Item className="form-item">
+        <AntdForm.Item>
           <Checkbox indeterminate={indeterminate} onChange={onCheckAllChange} checked={checkAll}>
             Check all
           </Checkbox>
@@ -193,12 +190,10 @@ const AddKoiFish: React.FC = () => {
             value={checkedList}
             onChange={onChange}
           />
-        </Form.Item>
+        </AntdForm.Item>
 
-        <Form.Item className="form-item">
-          <button type="submit" className="btn-primary">Submit</button>
-        </Form.Item>
-      </Form>
+        <button type="submit" className="btn-primary">Submit</button>
+      </form>
 
       <Modal show={showModal} onHide={handleCloseModal}>
         <Modal.Header closeButton>
@@ -207,13 +202,9 @@ const AddKoiFish: React.FC = () => {
         <Modal.Body>{modalMessage}</Modal.Body>
         <Modal.Footer>
           {modalNotification === "Success" ? (
-            <Button variant="primary" onClick={handleGoToLogin}>
-              Go to Login
-            </Button>
+            <Button variant="primary" onClick={handleGoToLogin}>Go to Login</Button>
           ) : (
-            <Button variant="secondary" onClick={handleCloseModal}>
-              Close
-            </Button>
+            <Button variant="secondary" onClick={handleCloseModal}>Close</Button>
           )}
         </Modal.Footer>
       </Modal>
