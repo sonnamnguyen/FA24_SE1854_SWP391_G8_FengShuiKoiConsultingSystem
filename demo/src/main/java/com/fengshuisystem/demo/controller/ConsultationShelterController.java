a9f1dfec8ff7f125597c6446d0d7b34521b4e895
khoipdse184586 <khoipdse184586@fpt.edu.vn>
2024-11-09 19:07:24 +0700

    Save current changes before rebase

--- demo/src/main/java/com/fengshuisystem/demo/controller/ConsultationShelterController.java
+++ demo/src/main/java/com/fengshuisystem/demo/controller/ConsultationShelterController.java
@@ -1,13 +1,18 @@
 package com.fengshuisystem.demo.controller;
 
 import com.fengshuisystem.demo.dto.ApiResponse;
+import com.fengshuisystem.demo.dto.ConsultationAnimalDTO;
 import com.fengshuisystem.demo.dto.ConsultationShelterDTO;
+import com.fengshuisystem.demo.dto.PageResponse;
 import com.fengshuisystem.demo.service.impl.ConsultationShelterServiceImpl;
+import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/consultation-shelters")
@@ -29,6 +34,38 @@ public class ConsultationShelterController {
                 .result(result)
                 .code(1000)
                 .message("Consultation Shelter created successfully")
+                .build();
+    }
+
+    @GetMapping
+    public ApiResponse<PageResponse<ConsultationShelterDTO>> getAllConsultationShelterPage(
+            @RequestParam(value = "page", required = false, defaultValue = "1") int page,
+            @RequestParam(value = "size", required = false, defaultValue = "10") int size
+    ) {
+
+        return ApiResponse.<PageResponse<ConsultationShelterDTO>>builder()
+                .result(consultationShelterService.getAllConsultationShelterPage(page, size))
+                .build();
+    }
+
+    @PutMapping("/{id}")
+    public ApiResponse<ConsultationShelterDTO> updateConsultationShelter(@PathVariable Integer id, @Valid @RequestBody ConsultationShelterDTO consultationShelterDTO) {
+        return ApiResponse.<ConsultationShelterDTO>builder()
+                .result(consultationShelterService.updateConsultationShelter(id, consultationShelterDTO))
+                .build();
+    }
+
+    @GetMapping("/{id}")
+    public ApiResponse<ConsultationShelterDTO> getConsultationShelterById(@PathVariable Integer id) {
+        return ApiResponse.<ConsultationShelterDTO>builder()
+                .result(consultationShelterService.getConsultationShelterById(id))
+                .build();
+    }
+
+    @GetMapping("/animalCategory")
+    public ApiResponse<List<ConsultationShelterDTO>> getAllConsultationShelter() {
+        return ApiResponse.<List<ConsultationShelterDTO>>builder()
+                .result(consultationShelterService.getAllConsultationShelter())
                 .build();
     }
 }
