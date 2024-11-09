3fe378d0c15c0688be78babf2c552dd89fac7107
Nguyễn Hoàng Đức Minh <minhnhdse181549@fpt.edu.vn>
2024-11-09 07:13:04 +0700

    small change on Post

--- demo/src/main/java/com/fengshuisystem/demo/repository/PostRepository.java
+++ demo/src/main/java/com/fengshuisystem/demo/repository/PostRepository.java
@@ -11,6 +11,8 @@ public interface PostRepository extends JpaRepository<Post, Integer> {
     Page<Post> findAllByStatus(Status status, Pageable pageable);
     @Query(value = "SELECT c from Post c where c.status = 'ACTIVE'")
     Page<Post> findAllByPostCategory(String postCategory, Pageable pageable);
+    @Query(value = "SELECT c from Post c where c.status = 'ACTIVE'")
     Page<Post> findAllByAccount_Email(String email, Pageable pageable);
+    @Query(value = "SELECT c from Post c where c.status = 'ACTIVE'")
     Page<Post> findAllByTitleContaining(String title, Pageable pageable);
 }
