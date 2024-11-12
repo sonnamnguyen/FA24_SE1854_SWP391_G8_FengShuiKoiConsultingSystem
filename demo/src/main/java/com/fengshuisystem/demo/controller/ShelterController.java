55e08043e3a5050d90339ea0c19596e37a0031f5
Huypq-SE <huypqse184931@fpt.edu.vn>
2024-11-12 10:55:36 +0700

    update

--- demo/src/main/java/com/fengshuisystem/demo/controller/ShelterController.java
+++ demo/src/main/java/com/fengshuisystem/demo/controller/ShelterController.java
@@ -52,7 +52,7 @@ public class ShelterController {
             .result(shelterService.getSheltersBySearch(name, page, size))
             .build();
   }
-  @GetMapping("/shelter-searchtui p")
+  @GetMapping("/shelter-search")
   public ApiResponse<PageResponse<ShelterCategoryDTO>> getShelterByDestiny(
           @RequestParam List<String> destiny,
           @RequestParam(value = "page", required = false, defaultValue = "1") int page,
